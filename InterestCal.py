47c20cb08284ad572bbcb671b54c012e0c2270e3
skye92 <gamecoad92@gmail.com>
2021-02-03 22:25:14 -0600

    Add files via upload
    This is the second version of my dividend calculator. It still is not functioning correctly but it is far closer than the first attempt

--- InterestCal.py
+++ InterestCal.py
@@ -1,36 +1,42 @@
-def interest_cal():
-    user_list = []
+def account_balance():# def function/ what is function name, how many params.
+    starting_balance = float(input("what is the starting balance? ")) # ask user for input for starting_balance
 
-    user_input = float(input("please enter a starting amount"))
+    stock_cost = float(input("how much does the stock cost? "))# ask user how much the stock cost
 
-    user_list.append(user_input)
+    stock_owned = starting_balance / stock_cost # how many of the stock is owned 
 
-    starting_balance = user_list
+    dividend_amount = float(input("how much is the dividend")) #How much is the dividend 
 
-    # starting_balance = input("how much is the starting balance?")
+    dividend_payment = stock_owned * dividend_amount # how much is the dividend payment 
 
-    new_monthly_balance = 0
+    new_balance = starting_balance + dividend_payment # what is the new balance of the starting balance and the dividend payment 
+    
+    print(new_balance) # print new balance 
 
-    my_balance = (starting_balance) + (new_monthly_balance)
+    balance_list = []
 
-    cost_of_share = input("how much does the stock cost?")
+    balance_list.append(new_balance)
 
-    shares_owned = float(cost_of_share) / my_balance
+    #create list that adds new_balance to it. 
 
-    dividend_payout = input("how much is the monthly divided payment?")
+    # define nested function 
+    def interest_cal():
+        count = 0
+        how_many = int(input("How many times? "))
 
-    interest = shares_owned * float(dividend_payout)
+        while(count <= how_many):
+            comp_interst = new_balance + dividend_payment # new balance + dividend payment is = to ? 
+            print(comp_interst)
+            for i in balance_list:
+                i = i + comp_interst
+                print(i)
+                count = count + 1
+# how many times to add the new balance to dividend payment 
 
-    new_monthly_balance = shares_owned + interest
+# print results of new balance + dividend payment * how many times 
+    interest_cal()
 
-    return(new_monthly_balance)
 
-interst_call_results = interest_cal()
 
-count= 0
+account_balance()
 
-how_many = int(input("how many units of time?"))
-
-while(count <= int(how_many)):
-        print(interst_call_results)
-        count = count + 1
