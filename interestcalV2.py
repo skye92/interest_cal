2d7b7fe8dd8ec278025b78d3740fad811cfaa5e6
skye92 <gamecoad92@gmail.com>
2021-02-06 23:12:41 -0600

    Add files via upload

---
+++ interestcalV2.py
@@ -0,0 +1,14 @@
+starting_balance = 1500
+stock_cost = 10
+stock_amount = starting_balance / stock_cost
+dividend = .07
+dividend_payment = stock_amount * dividend
+
+result = starting_balance + dividend_payment
+
+balance_list = [result]
+
+for i in range (len(balance_list)):
+    balance_list.append(balance_list)
+
+    print(balance_list)
